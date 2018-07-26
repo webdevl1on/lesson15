68a7cade741d0940459fa4f84e3d29b7105083fb
Lysenko Evhen <lzhekal27@gmail.com>
2018-07-26 21:52:45 +0200

    1st

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var	btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var main = document.getElementById('main');
+var myList = document.getElementById('list');
+var ul = document.getElementsByTagName('ul');
+var story = myList.getElementsByClassName('item');
+var popup =	document.getElementsByClassName('popup');
+var close = document.querySelector('.close');
+
+function addItem() {
+	var newLi = document.createElement('li');
+	newLi.innerHTML = 'Новая задача';
+	newLi.className = 'item';
+	myList.appendChild(newLi);
+	popup[0].style.display = "none";	
+}
+
+function delItem() {
+	myList.removeChild(story[0]);
+
+	if(story.length == 0) {
+		popup[0].style.display = 'block';
+	}
+
+}
+
+function closePopup() {
+	popup[0].style.display = "none";
+}
+
+btn1.addEventListener('click', addItem);
+btn2.addEventListener('click', delItem);
+close.addEventListener('click', closePopup);
+
+
+
+
